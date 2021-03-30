d8e65cc830d0b123ce8f655e36540b02ffe53db8
Robert Marquess <qowsrule@gmail.com>
2021-03-30 01:06:01 -0400

    Add images to news articles.

--- js/news.js
+++ js/news.js
@@ -1,15 +1,18 @@
 const news = [
     {
         "heading": "Article Loaded From JS",
-        "excerpt": "This news article and those that follow are loaded from news.js!"
+        "excerpt": "This news article and those that follow are loaded from news.js!",
+        "image": "img/placeimg_100_100_animals_1.jpg"
     },
     {
         "heading": "A Second Article",
-        "excerpt": "Because the first article can't be by itself."
+        "excerpt": "Because the first article can't be by itself.",
+        "image": "img/placeimg_100_100_animals_2.jpg"
     },
     {
         "heading": "Third Time's The Charm",
-        "excerpt": "We can have as many articles as we want!"
+        "excerpt": "We can have as many articles as we want!",
+        "image": "img/placeimg_100_100_animals_3.jpg"
     }
 ];
 const newsElement = document.querySelector('.news');
@@ -18,8 +21,9 @@ let newsHTML = '';
 news.forEach(function(article) {
     newsHTML += `
         <article>
+            <img src="${article.image}">
             <h3>${article.heading}</h3>
-            <p>${article.excerpt}</p>
+            <p>${article.excerpt} <a href="#">Read more...</a></p>
         </article>
     `;
 });
